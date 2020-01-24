32208fa274a538496c29cbdf7d76df22c95f4c13
ryunosukemaeda0922 <ryunosuke.maeda@vitaly-cloud.com>
2020-01-25 03:04:40 +0900

    yarn add minireset.css

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Layout } from "antd";
 import MyHeader from "./components/MyHeader/MyHeader";
 import MyFooter from "./components/MyFooter/MyFooter";
 
+import "minireset.css";
+
 class App extends Component {
   render() {
     return (
