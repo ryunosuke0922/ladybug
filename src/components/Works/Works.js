58074d6f2998e3db4505132febe5986254b41d1f
ryunosukemaeda0922 <ryunosuke.maeda@vitaly-cloud.com>
2020-01-26 18:18:20 +0900

    fix

--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography, Layout } from "antd";
+import { Typography, Layout, List } from "antd";
 import styled from "styled-components";
 
 const { Title, Text } = Typography;
@@ -16,48 +16,36 @@ const WorksTitle = styled(Title)`
   letter-spacing: 4px;
 `;
 
-const WorksDate = styled(Text)`
-  font-size: 12px;
-  padding-left: 12px;
-`;
+const data1 = [
+  "Laravel | PHP 業務改善Webアプリケーション 3ヶ月",
+  "iOS / Android | ReactNative | JavaScript イベントアプリ 半年",
+  "iOS / Android | ReactNative | JavaScript イベントアプリ 1年"
+];
+
+const data2 = [
+  "iOS / Android | Objective-C / Java / C/C++ パチンコ、パチスロのネイティブアプリ開発",
+  "アニメ版権 | 独自基盤 | C/C++ 遊技機の液晶開発 1年半",
+  "オリジナル版権 | 独自基盤 | C/C++ 遊技機の液晶開発 1年半"
+];
 
 export const Works = () => (
   <Base>
     <WorksTitle level={3}>Works</WorksTitle>
     <div style={{ marginTop: "60px" }}>
       <Title level={4}>現職</Title>
-      <Text>
-        Laravel | PHP 業務改善Webアプリケーション
-        <WorksDate>3ヶ月</WorksDate>
-      </Text>
-      <br />
-      <Text>
-        iOS / Android | ReactNative | JavaScript イベントアプリ
-        <WorksDate>半年</WorksDate>
-      </Text>
-      <br />
-      <Text>
-        iOS / Android | ReactNative | JavaScript イベントアプリ
-        <WorksDate>1年</WorksDate>
-      </Text>
+      <List
+        bordered
+        dataSource={data1}
+        renderItem={item => <List.Item>{item}</List.Item>}
+      />
     </div>
     <div style={{ marginTop: "30px" }}>
-      <Title level={4}>会社員時代</Title>
-      <Text>
-        iOS / Android | Objective-C / Java / C/C++
-        パチンコ、パチスロのネイティブアプリ開発
-        <WorksDate>8ヶ月</WorksDate>
-      </Text>
-      <br />
-      <Text>
-        アニメ版権 | 独自基盤 | C/C++ 遊技機の液晶開発
-        <WorksDate>1年半</WorksDate>
-      </Text>
-      <br />
-      <Text>
-        オリジナル版権 | 独自基盤 | C/C++ 遊技機の液晶開発
-        <WorksDate>1年半</WorksDate>
-      </Text>
+      <Title level={4}>前職</Title>
+      <List
+        bordered
+        dataSource={data2}
+        renderItem={item => <List.Item>{item}</List.Item>}
+      />
     </div>
   </Base>
 );
